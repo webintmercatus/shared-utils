29497cdd0d4f59b626b0205ddd963de2a041005e
justin.dasilva <justin.dasilva@mercatus.com>
2022-05-13 15:16:14 -0400

    Published by publish-to-git 2d88c4b05f634c6370c6d8299fd6809d54453adc (HEAD -> refs/heads/task/update-source-commits, refs/remotes/origin/task/update-source-commits) update source commits

---
+++ dist/createGroup.d.ts
@@ -0,0 +1,3 @@
+import type { Configs, NestedConfigs } from '@config-manager/types';
+declare const createGroup: (group: string, configs: NestedConfigs) => Configs;
+export { createGroup };
---
+++ dist/createOnboarderDefinition.d.ts
@@ -0,0 +1,3 @@
+import type { Configs, SourceOnboarderDefinition } from '@config-manager/types';
+declare const createOnboarderDefinition: (onboarder: any, name: string, configs: Configs) => SourceOnboarderDefinition;
+export { createOnboarderDefinition };
