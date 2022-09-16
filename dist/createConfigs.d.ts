055f9e63a3f75245d013c31c57949ce352524cac
Justin Da Silva <justin.dasilva@mercatus.com>
2022-09-15 20:07:15 -0400

    Published by publish-to-git ab32d01a5adf567d7dd4939c030463930560cf10 (HEAD -> refs/heads/task/shared-utils-update-to-use-github, refs/remotes/origin/task/CONFMG-64-api-key-in-config-manager, refs/remotes/origin/master, refs/remotes/origin/HEAD, refs/heads/master) Pull request #136: Task/CONFMG-65 helpful husky

---
+++ dist/createConfigs.d.ts
@@ -0,0 +1,3 @@
+import type { Configs, NestedConfigs } from './types';
+declare const createConfigs: (obj: NestedConfigs) => Configs;
+export { createConfigs };
---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+export * from './constants';
+export * from './fetchConfigs';
+export * from './types';
+export * from './createConfigsFromJson';
+export * from './parseSource';
